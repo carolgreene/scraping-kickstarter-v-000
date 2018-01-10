3232122ad79e75500c694c67dae59061e6841f95
Carol Greene <carolgreene3@gmail.com>
2018-01-10 23:52:10 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -21,7 +21,7 @@ def create_project_hash
   #Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    binding.pry 
+    binding.pry
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
@@ -32,3 +32,4 @@ def create_project_hash
   #return the project hash
   projects
 end
+create_project_hash
